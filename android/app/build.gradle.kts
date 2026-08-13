1bfab2b131a072051a743a6fa352b0c4ace22572
unstopablejay <96796539+unstopablejay@users.noreply.github.com>
2026-08-13 13:58:15 +0530

    rename

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ val keystoreProperties = Properties().apply {
 }
 
 android {
-    namespace = "com.example.expense_manager"
+    namespace = "com.TU.expense.manager"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -28,8 +28,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.expense_manager"
+        applicationId = "com.TU.expense.manager"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/TU/expense/manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/TU/expense/manager/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.expense_manager
+package com.TU.expense.manager
 
 import io.flutter.embedding.android.FlutterActivity
 
