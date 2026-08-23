7edff104ac865871eb4a0b07b7c5c67fde5d417b
unstopablejay <96796539+unstopablejay@users.noreply.github.com>
2026-08-23 18:58:00 +0530

    ci: remove redundant import in custom_category_icons_test (#30)

--- test/custom_category_icons_test.dart
+++ test/custom_category_icons_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tu_expense_tracker/main.dart';
-import 'package:tu_expense_tracker/src/core/snapshot_store.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
